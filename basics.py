f6b3a22beb08e5dd4f93f02eee03a12e78a78717
Neel <neel.gorasiya@fxis.ai>
2024-12-19 15:31:17 +0530

    basic operations

---
+++ basics.py
@@ -0,0 +1,23 @@
+# Execution order
+print("This")
+print("code is")
+print("interpreted")
+
+# data types
+print('words')
+print("words")
+print(123)
+print(-25)
+print(0.35)
+print(10 + 0.3418525)
+print(5 * 'hello\n')
+print(10 - 3 * 4 + 5)
+
+# variables
+test_value = 123
+test_value += 22
+print(test_value)
+
+# input
+user_input = input("Please enter your name:")
+print('Hello  ', user_input)
---
+++ functions_methods.py
@@ -0,0 +1,13 @@
+# functions
+print("Print func")
+
+# methods -> functions of datatypes
+print('value'.upper())
+print("VALUE".lower())
+print('value'.replace('e', '3'))
+
+# New functions
+print(abs(-1325))
+print(max(1, 2))
+print(min(1 , 2))
+print(len('test'))
